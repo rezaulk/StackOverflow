d8254bbebce552f862bddb151c153db428c07514
MD. REZA UL KARIM <rezaul.bd77@gmail.com>
2024-07-27 20:08:34 +0600

    Add project files.

---
+++ How to get environment name in Program.cs file in .NET 8/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using System;
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", true, true)
+
+    .Build();
+
+string exampleSetting = configuration["ExampleSetting"];
+Console.WriteLine($"Example Setting: {exampleSetting}");
